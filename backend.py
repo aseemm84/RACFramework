ff1b7dee662dafd96e57410af7711e03b01a3c93
Aseem Mehrotra <67800004+aseemm84@users.noreply.github.com>
2024-11-05 17:30:11 +0530

    Add files via upload

---
+++ backend.py
@@ -0,0 +1,40 @@
+import streamlit as st
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains import LLMChain
+
+
+groq = st.secrets["Groq_API_Key"]
+
+
+
+llm = ChatGroq(
+    model="llama-3.1-70b-versatile",
+    groq_api_key=groq,
+    temperature=0
+    # other params...
+)
+
+def main_prompt(text_string):
+    template = """
+            I need you to help me create impactful bullet points for my resume. I will provide you with text {text_string} describing a task or accomplishment from my professional career.
+            For each piece of text I provide, please:
+            1. Analyze the text and extract key results, actions, and context.
+            2. Create concise bullet points (no more than one line each) in the Result - Action - Context (RAC) format.
+            3. Start each bullet point with a strong action verb. Prioritize impactful and dynamic verbs that emphasize achievements and skills.
+            4. Use relevant keywords throughout the bullet point to optimize for Applicant Tracking Systems (ATS).
+            5. If necessary, break down the provided text into multiple bullet points to ensure conciseness and clarity.
+            
+            Here's an example of the text that user will provide:
+            'Led a team of five engineers in developing a new machine learning algorithm that improved customer conversion rates by 15% within six months by analyzing user behavior patterns.'
+            I expect an output similar to this:
+            Improved customer conversion rates by 15% within six months by leading a team of five engineers in the development of a new machine learning algorithm that analyzed user behavior patterns.
+            """
+    prompt = ChatPromptTemplate.from_template(template)
+    chain = prompt | llm
+    try:
+        response = chain.invoke({"text_string": text_string})
+        return response.content
+    except Exception as e:
+        return f"Error: {str(e)}"
+
---
+++ frontend.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import PyPDF2
+from backend import main_prompt
+from PIL import Image
+import time
+import re
+import pandas as pd
+
+# Set page config as the first Streamlit command
+st.set_page_config(page_title="CV Bullet Points Creator", page_icon="📄")
+
+
+col1, col2 = st.columns([1, 4])
+
+logo = Image.open("logo.png")
+col1.image(logo, width=100)  # Adjust width as needed
+col2.title("CV Creator Pro: RAC Framework")
+
+text_string = st.text_area("Enter Your Achievement", height=200)
+
+
+# Evaluate button
+if st.button("Summarize"):
+    if text_string is None:
+        st.error("Please enter something to Summarize.")
+    else:
+        if text_string:
+                with st.spinner("Summarizing..."):
+                    progress_bar = st.progress(0)
+                    for i in range(100):
+                        time.sleep(0.05)  # Simulate processing time
+                        progress_bar.progress(i + 1)
+                    result = main_prompt(text_string)
+                    st.subheader("RAC Framework Result:")
+                    st.write(result)
+                
+
+st.sidebar.title("About")
+st.sidebar.info("""
+Welcome to the CV Bullet Point Creator, your expert assistant for crafting a standout resume!
+
+This tool analyzes your achievement against industry best practices, ensuring it meets the highest standards, and convert it to RAC framework bullet points.
+
+Unlock your career potential with a resume that truly reflects your skills and experiences!
+""")
+linkedin_url = "https://www.linkedin.com/in/aseem-mehrotra/"
+st.sidebar.markdown(f'<a href="{linkedin_url}" target="_blank"><img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" alt="LinkedIn" style="height: 30px;"></a>', unsafe_allow_html=True)
